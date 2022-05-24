201ac1704bad372d33d8f77c5617b5614b468707
Romero, Matias (partner) <matias.romero@radventure.com>
2022-05-24 15:38:55 -0300

    fix

--- VacunnasistBackend/Data/AppliedVaccineEntityTypeConfiguration.cs
+++ VacunnasistBackend/Data/AppliedVaccineEntityTypeConfiguration.cs
@@ -8,6 +8,8 @@ namespace VacunassistBackend.Data
     {
         public void Configure(EntityTypeBuilder<AppliedVaccine> builder)
         {
+            builder.HasKey(t => new { t.UserId, t.VaccineId });
+
             builder.HasOne(av => av.User).WithMany(u => u.Vaccines).HasForeignKey(av => av.UserId);
             builder.HasOne(av => av.Vaccine).WithMany(u => u.Users).HasForeignKey(av => av.VaccineId);
         }
