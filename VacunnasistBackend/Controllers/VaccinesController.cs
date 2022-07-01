4274dd9893494a940ac9a6fef5604b054193e081
Romero, Matias (partner) <matias.romero@radventure.com>
2022-07-01 20:30:40 -0300

    Vaccines report

--- VacunnasistBackend/Controllers/VaccinesController.cs
+++ VacunnasistBackend/Controllers/VaccinesController.cs
@@ -69,6 +69,65 @@ namespace VacunassistBackend.Controllers
         [Route("report-vaccines")]
         public IActionResult ReportVaccines()
         {
+            var date = DateTime.Today;
+            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1);
+            var applieds = _context.AppliedVaccines.Include(u => u.Vaccine)
+            .Where(x => x.Appointment != null && x.AppliedDate.HasValue && x.AppliedDate > firstDayOfMonth && x.AppliedDate < lastDayOfMonth)
+            .ToArray();
+            if (applieds.Any() == false)
+                throw new HttpResponseException(400, message: "No se han aplicado vacunas este mes");
+
+            var grouped = applieds.GroupBy(x => x.Vaccine.Id).OrderBy(x => x.Key).ToArray();
+
+            iTextSharp.text.Font _titleFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 18, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+            iTextSharp.text.Font _subtitle = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 15, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+            iTextSharp.text.Font _standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 13, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+
+            var tempFolder = _configuration["TempFolder"];
+            if (Directory.Exists(tempFolder) == false)
+                Directory.CreateDirectory(tempFolder);
+
+            var randomName = "report_vaccines_" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            var path = Path.Combine(tempFolder, randomName + ".pdf");
+            var text = new StringBuilder();
+
+            Document doc = new Document(PageSize.A4);
+            PdfWriter writer = PdfWriter.GetInstance(doc,
+                                        new FileStream(path, FileMode.Create));
+            doc.AddTitle("Reporte de dosis");
+            doc.Open();
+
+            var currentMonth = DateTime.Now.ToString("MMMM", CultureInfo.CreateSpecificCulture("es"));
+            var p1 = new Paragraph("VACUNASSIST", _titleFont);
+            p1.Alignment = Element.ALIGN_CENTER;
+            var p2 = new Paragraph("Reporte de dosis por vacunas en el mes actual (" + currentMonth + ")", _subtitle);
+            p2.Alignment = Element.ALIGN_CENTER;
+
+            doc.Add(p1);
+            doc.Add(p2);
+            doc.Add(Chunk.NEWLINE);
+
+            List list = new List(List.UNORDERED, 20f);
+            list.IndentationLeft = 20f;
+            list.PreSymbol = "*";
+            foreach (var g in grouped)
+            {
+                list.Add(string.Format("Vacuna: {0} - {1} dosis aplicadas", g.First().Vaccine.Name, g.Count()));
+            }
+
+            doc.Add(list);
+            doc.Add(Chunk.NEWLINE);
+            doc.Add(new Paragraph("Fecha reporte: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"), _standardFont));
+            doc.Add(Chunk.NEWLINE);
+            var barcode = new BarcodeQRCode("www.vacunassist.com", 100, 100, null);
+            iTextSharp.text.Image imgBarCode = barcode.GetImage();
+            imgBarCode.SetAbsolutePosition(483, 740);
+            doc.Add(imgBarCode);
+
+            doc.Close();
+            writer.Close();
+
             return Ok(new
             {
                 message = "Reporte creado correctamente"
